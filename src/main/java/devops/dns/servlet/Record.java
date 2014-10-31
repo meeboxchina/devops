6b60b61744801a3134b48497ba01b9bd68070b93
Sunny Sun <sunyu.cn@gmail.com>
2014-10-31 19:13:48 +0800

    json

--- src/main/java/devops/dns/servlet/Record.java
+++ src/main/java/devops/dns/servlet/Record.java
@@ -13,6 +13,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  * Servlet implementation class Record
  */
@@ -56,6 +59,38 @@ public class Record extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		String zone = request.getParameter("zone");
+		if(zone==null || zone==""){
+			return;
+		}
+		devops.dns.Record record = new devops.dns.Record();
+		List<HashMap> list = record.getZoneRecords(zone);
+
+		Iterator itZone = list.iterator();
+		
+		JSONObject json = new JSONObject();
+		json.put("status", "ok");
+		json.put("message", "get records list");
+		JSONArray jsonarray = new JSONArray();
+		while(itZone.hasNext()){
+			HashMap recordHM = (HashMap) itZone.next();
+			
+			JSONObject jsonrecord = new JSONObject();
+			jsonrecord.put("host", recordHM.get("host"));
+			jsonrecord.put("view", recordHM.get("view"));
+			jsonrecord.put("data", recordHM.get("data"));
+			jsonrecord.put("ttl", recordHM.get("ttl"));
+			jsonrecord.put("type", recordHM.get("type"));
+			
+			jsonarray.put(jsonrecord);
+			
+			
+			
+		}
+		json.put("data", jsonarray);
+		
+		out.println(json.toString());
 	}
 
 }
